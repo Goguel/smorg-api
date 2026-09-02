6d082cf2344dcc14bcb076708e6f5fd9585f49c6
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2026-09-01 21:01:41 -0300

    Adiciona esqueleto Ktor, go.mod e configurações do mise

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+}
+
+group = "com.smorg"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.cors)
+    implementation(ktorLibs.server.netty)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,5 @@
+package com.smorg
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
